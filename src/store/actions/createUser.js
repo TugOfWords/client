8eb6f2cdf73ac5fd2046e971b898eb0bf79d5ec3
Daniel Kim <jicircle@naver.com>
2018-02-11 15:12:44 -0500

    Remove user from database on removeUser call (#7)

--- src/store/actions/createUser.js
+++ src/store/actions/createUser.js
@@ -38,12 +38,16 @@ export const createUserSuccess = (uid, username) => ({
   username,
 });
 
+
 /**
  * Handle the REMOVE_USER action
  * @returns {Object}
  *   the data for the REMOVE_USER action
  */
 export const removeUser = () => {
+  const uid = localStorage.getItem('uid');
+  const userData = { uid };
+  socket.emit('removeUser', userData);
   localStorage.removeItem('uid');
   localStorage.removeItem('username');
   localStorage.removeItem('currentRoom');
